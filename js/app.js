a2407fed1fa92f13baf4c7721c20aa280726edc7
DanielleColucci <ddcolucci99@gmail.com>
2022-12-07 21:01:21 -0500

    add cached element references in js

---
+++ js/app.js
@@ -0,0 +1,5 @@
+/* ------------ CACHED ELEMENT REFERENCES ------------ */
+const inputEl = document.getElementById('todo-input')
+const btnEl = document.getElementById('submit-button')
+const ulEl = document.getElementById('todo-list')
+
